bd2812463c3ded4db71728c1c6ab60f46c827523
Pranav Mahajan <pranavmahajan.4122005@gmail.com>
2026-06-23 10:42:14 +0530

    Update WhatsAppButton.tsx

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function WhatsAppButton() {
-  const number = '9784853101'
+  const number = '9785843101'
   const message = encodeURIComponent('Hello Capt. Mahajan & Sons, I would like to enquire about your products.')
   const href = `https://wa.me/${number}?text=${message}`
 
